0231e23a18c926f94869b5827524b8c0408d613e
poveda_g <guillaume.poveda@gmail.com>
2022-05-17 17:57:52 +0200

    Document the generic_tools/do_problem and generic_tools/do_solver modules

--- discrete_optimization/generic_tools/do_solver.py
+++ discrete_optimization/generic_tools/do_solver.py
@@ -1,3 +1,5 @@
+"""Minimal API for a discrete-optimization solver."""
+
 from abc import abstractmethod
 
 from discrete_optimization.generic_tools.result_storage.result_storage import (
@@ -6,6 +8,15 @@ from discrete_optimization.generic_tools.result_storage.result_storage import (
 
 
 class SolverDO:
+    """Base class for a discrete-optimization solver."""
     @abstractmethod
     def solve(self, **kwargs) -> ResultStorage:
+        """Generic solving function.
+
+        Args:
+            **kwargs: any argument specific to the solver
+
+        Returns (ResultStorage): a result object containing potentially a pool of solutions
+        to a discrete-optimization problem
+        """
         ...
