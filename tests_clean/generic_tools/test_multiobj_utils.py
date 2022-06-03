f5debf477df870fcebdcb77479f3987b736a4130
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2022-06-03 10:12:38 +0200

    Move some tests for generic_tools

---
+++ tests_clean/generic_tools/test_multiobj_utils.py
@@ -0,0 +1,25 @@
+import numpy as np
+from discrete_optimization.generic_tools.result_storage.multiobj_utils import (
+    TupleFitness,
+)
+
+
+def test_tuplefitness():
+    fitness_1 = TupleFitness(np.array([1, 2]), 2)
+    fitness_2 = TupleFitness(np.array([2, 3]), 2)
+    fitness_3 = TupleFitness(np.array([0, 3]), 2)
+    assert fitness_1 <= fitness_2
+    assert fitness_1 < fitness_2
+    assert not (fitness_1 == fitness_2)
+    assert not (fitness_1 > fitness_2)
+    assert not (fitness_1 >= fitness_2)
+    assert fitness_1 == fitness_3
+    assert fitness_3 < fitness_2
+    assert fitness_3 <= fitness_2
+    assert not (fitness_3 > fitness_2)
+    assert not (fitness_3 >= fitness_2)
+    assert not (fitness_3 == fitness_2)
+
+
+if __name__ == "__main__":
+    test_tuplefitness()
