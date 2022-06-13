c45795d7a8855a606573aa7a579ecfca4b54d1b2
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2022-06-13 16:06:35 +0200

    Fix test_load

--- tests/tsp/tsp_model_test.py
+++ tests/tsp/tsp_model_test.py
@@ -9,16 +9,17 @@ def test_load():
     files = get_data_available()
     file = [f for f in files if "85900" in f][0]
     one_model = parse_file(file)
-    basic_solution = SolutionTSP(0, 0, list(range(one_model.node_count)), None, None)
-    for k in range(100):
-        compute_length(
-            0,
-            0,
-            basic_solution.permutation,
-            one_model.list_points,
-            one_model.node_count,
-            one_model.length_permutation,
-        )
+    basic_solution = SolutionTSP(
+        problem=one_model, permutation=list(range(1, one_model.node_count))
+    )
+    compute_length(
+        start_index=basic_solution.start_index,
+        end_index=basic_solution.end_index,
+        solution=basic_solution.permutation,
+        list_points=one_model.list_points,
+        node_count=one_model.node_count,
+        length_permutation=one_model.length_permutation,
+    )
 
 
 if __name__ == "__main__":
