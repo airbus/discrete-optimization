d9e42553261121806089dc86d1a1330a15da7ff5
Nolwen <huet@imacs.polytechnique.fr>
2022-06-13 11:44:01 +0200

    Skip test_facitlity_cp on windows
    Crashing for now, to be investigate

--- tests_clean/facility/test_facility_cp.py
+++ tests_clean/facility/test_facility_cp.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 import pytest
 from discrete_optimization.facility.facility_parser import (
@@ -12,6 +13,7 @@ from discrete_optimization.facility.solvers.facility_cp_solvers import (
 )
 
 
+@pytest.mark.skipif(sys.platform.startswith("win"), reason="Not yet working on windows")
 def test_facility_cp():
     file = [f for f in get_data_available() if os.path.basename(f) == "fl_16_1"][0]
     print(file)
