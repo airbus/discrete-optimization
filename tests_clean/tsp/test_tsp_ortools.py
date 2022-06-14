a8de16a555aa817bd7637e09add10b27c105c542
Nolwen <huet@imacs.polytechnique.fr>
2022-06-14 14:55:22 +0200

    Rename tsp tests to be found by pytest and keep some consistency
    And move them to tests_clean

--- tests_clean/tsp/test_tsp_ortools.py
+++ tests_clean/tsp/test_tsp_ortools.py
@@ -6,7 +6,7 @@ from discrete_optimization.tsp.solver.solver_ortools import TSP_ORtools
 from discrete_optimization.tsp.tsp_parser import get_data_available, parse_file
 
 
-def run_ortools():
+def test_ortools():
     files = get_data_available()
     files = [f for f in files if "tsp_200_2" in f]
     model = parse_file(files[0], start_index=0, end_index=10)
@@ -19,4 +19,4 @@ def run_ortools():
 
 
 if __name__ == "__main__":
-    run_ortools()
+    test_ortools()
