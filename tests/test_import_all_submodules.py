eb5b58390b39d4efca9deb1c70549f3c9ff39578
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2022-06-30 10:45:11 +0200

    Add a test which try importing all submodules
    (Some modules were not even imported during the tests until now)

---
+++ tests/test_import_all_submodules.py
@@ -0,0 +1,46 @@
+import importlib
+import pkgutil
+import sys
+
+import discrete_optimization
+
+
+def find_abs_modules(package):
+    """Find names of all submodules in the package
+
+    https://stackoverflow.com/questions/48879353/how-do-you-recursively-get-all-submodules-in-a-python-package
+
+    """
+    path_list = []
+    spec_list = []
+    for importer, modname, ispkg in pkgutil.walk_packages(package.__path__):
+        if modname == "hub.__skdecide_hub_cpp":
+            continue
+        import_path = f"{package.__name__}.{modname}"
+        if ispkg:
+            spec = pkgutil._get_spec(importer, modname)
+            try:
+                importlib._bootstrap._load(spec)
+                spec_list.append(spec)
+            except Exception as e:
+                print(f"Could not load package {modname}, so it will be ignored ({e}).")
+        else:
+            path_list.append(import_path)
+    for spec in spec_list:
+        del sys.modules[spec.name]
+    return path_list
+
+
+def test_importing_all_submodules():
+    modules_with_errors = []
+    for m in find_abs_modules(discrete_optimization):
+        try:
+            importlib.import_module(m)
+        except Exception as e:
+            modules_with_errors.append(m)
+            print(f"{m}: {e.__class__.__name__}: {e}")
+    if len(modules_with_errors) > 0:
+        raise ImportError(
+            f"{len(modules_with_errors)} submodules of discrete_optimization cannot be imported\n"
+            + f"{modules_with_errors}"
+        )
