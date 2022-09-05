6e68f54696be208d07aa802be3435bd457728d59
poveda_g <guillaume.poveda@airbus.com>
2022-09-05 11:16:10 +0200

    removed deprecated example

--- examples/facility/gphh_facility_example.py
+++ examples/facility/gphh_facility_example.py
@@ -3,15 +3,6 @@ from discrete_optimization.facility.facility_parser import (
     get_data_available,
     parse_file,
 )
-from discrete_optimization.facility.facility_solvers import (
-    FacilityCP,
-    GreedySolverDistanceBased,
-    GreedySolverFacility,
-    LP_Facility_Solver_PyMip,
-    solve,
-    solvers_map,
-)
-from discrete_optimization.facility.solvers.facility_cp_solvers import FacilityCPModel
 from discrete_optimization.facility.solvers.gphh_facility import GPHH, ParametersGPHH
 
 
@@ -24,36 +15,14 @@ def run_gphh():
     params_gphh.n_gen = 50
     params_gphh.min_tree_depth = 1
     params_gphh.max_tree_depth = 6
-    gphh_solver = GPHH(
-        training_domains=[model], domain_model=model, params_gphh=params_gphh
-    )
+    gphh_solver = GPHH(training_domains=[model],
+                       domain_model=model,
+                       params_gphh=params_gphh)
     gphh_solver.init_model()
     rs = gphh_solver.solve()
     sol, fit = rs.get_best_solution_fit()
     print(fit)
 
 
-def run_greedy():
-    model: FacilityProblem = parse_file(get_data_available()[5])
-    for method in [
-        GreedySolverFacility,
-        GreedySolverDistanceBased,
-        LP_Facility_Solver_PyMip,
-    ]:
-        result = solve(method, model, **solvers_map[method][1])
-        print(method, result.get_best_solution_fit()[1])
-    solver = FacilityCP(model)
-    solution, fit = solver.solve_lns(
-        fraction_to_fix=0.65,
-        nb_iteration=2,
-        limit_time_s=20,
-        greedy_start=True,
-        cp_model=FacilityCPModel.DEFAULT_INT_LNS,
-    )
-    print(fit)
-    print(solver.aggreg_sol(solution))
-    # 889538
-
-
 if __name__ == "__main__":
-    run_greedy()
+    run_gphh()
