fed3696114091677d1781fe819b2b46c75395b8b
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2022-09-06 10:19:55 +0200

    Add subclasses for MilpSolver depending if it wraps pymip or gurobi
    The idea is to put in common the solve method and also some utility
    functions that depends only on self.model being a pymip or a gurobi
    Model.
    
    Doing this, we avoid missing some parameters from parameters_milp
    in solve() like it is in some of them.

--- discrete_optimization/generic_tools/lp_tools.py
+++ discrete_optimization/generic_tools/lp_tools.py
@@ -1,6 +1,7 @@
+import logging
 from abc import abstractmethod
 from enum import Enum
-from typing import Optional
+from typing import Any, Optional
 
 import mip
 
@@ -8,6 +9,16 @@ from discrete_optimization.generic_tools.do_solver import SolverDO
 from discrete_optimization.generic_tools.result_storage.result_storage import (
     ResultStorage,
 )
+
+try:
+    import gurobipy
+except ImportError:
+    gurobi_available = False
+else:
+    gurobi_available = True
+
+
+logger = logging.getLogger(__name__)
 
 
 class MilpSolverName(Enum):
@@ -50,6 +61,8 @@ class ParametersMilp:
 
 
 class MilpSolver(SolverDO):
+    model: Optional[Any]
+
     @abstractmethod
     def init_model(self, **args):
         ...
@@ -63,3 +76,138 @@ class MilpSolver(SolverDO):
         self, parameters_milp: Optional[ParametersMilp] = None, **args
     ) -> ResultStorage:
         ...
+
+    @abstractmethod
+    def get_var_value_for_ith_solution(self, var: Any, i: int):
+        """Get value for i-th solution of a given variable."""
+        pass
+
+    @abstractmethod
+    def get_obj_value_for_ith_solution(self, i: int):
+        """Get objective value for i-th solution."""
+        pass
+
+    @property
+    @abstractmethod
+    def nb_solutions(self) -> int:
+        """Number of solutions found by the solver."""
+        pass
+
+
+class PymipMilpSolver(MilpSolver):
+    """Milp solver wrapping a solver from pymip library."""
+
+    model: Optional[mip.Model] = None
+
+    def solve(
+        self, parameters_milp: Optional[ParametersMilp] = None, **kwargs
+    ) -> ResultStorage:
+        if parameters_milp is None:
+            parameters_milp = ParametersMilp.default()
+        if self.model is None:
+            self.init_model(**kwargs)
+            if self.model is None:
+                raise RuntimeError(
+                    "self.model must not be None after self.init_model()."
+                )
+        self.model.max_mip_gap = parameters_milp.mip_gap
+        self.model.max_mip_gap_abs = parameters_milp.mip_gap_abs
+        self.model.sol_pool_size = parameters_milp.pool_solutions
+
+        self.model.optimize(
+            max_seconds=parameters_milp.time_limit,
+            max_solutions=parameters_milp.n_solutions_max,
+        )
+
+        logger.info(f"Solver found {self.model.num_solutions} solutions")
+        logger.info(f"Objective : {self.model.objective_value}")
+
+        return self.retrieve_solutions(parameters_milp=parameters_milp)
+
+    def get_var_value_for_ith_solution(self, var: mip.Var, i: int):  # type: ignore # avoid isinstance checks for efficiency
+        """Get value for i-th solution of a given variable."""
+        return var.xi(i)
+
+    def get_obj_value_for_ith_solution(self, i: int):
+        """Get objective value for i-th solution."""
+        if self.model is None:  # for mypy
+            raise RuntimeError(
+                "self.model should not be None when calling this method."
+            )
+        return self.model.objective_values[i]
+
+    @property
+    def nb_solutions(self) -> int:
+        """Number of solutions found by the solver."""
+        if self.model is None:
+            return 0
+        else:
+            return self.model.num_solutions
+
+
+class GurobiMilpSolver(MilpSolver):
+    """Milp solver wrapping a solver from gurobi library."""
+
+    model: Optional["gurobipy.Model"] = None
+
+    def solve(
+        self, parameters_milp: Optional[ParametersMilp] = None, **kwargs
+    ) -> ResultStorage:
+        if self.model is None:
+            self.init_model(**kwargs)
+            if self.model is None:  # for mypy
+                raise RuntimeError(
+                    "self.model must not be None after self.init_model()."
+                )
+        if parameters_milp is None:
+            parameters_milp = ParametersMilp.default()
+        self.model.setParam(gurobipy.GRB.Param.TimeLimit, parameters_milp.time_limit)
+        self.model.setParam(gurobipy.GRB.Param.MIPGapAbs, parameters_milp.mip_gap_abs)
+        self.model.setParam(gurobipy.GRB.Param.MIPGap, parameters_milp.mip_gap)
+        self.model.setParam(
+            gurobipy.GRB.Param.PoolSolutions, parameters_milp.pool_solutions
+        )
+        self.model.setParam("PoolSearchMode", parameters_milp.pool_search_mode)
+
+        self.model.optimize()
+
+        logger.info(f"Problem has {self.model.NumObj} objectives")
+        logger.info(f"Solver found {self.model.SolCount} solutions")
+        logger.info(f"Objective : {self.model.getObjective().getValue()}")
+
+        return self.retrieve_solutions(parameters_milp=parameters_milp)
+
+    def get_var_value_for_ith_solution(self, var: "gurobipy.Var", i: int):  # type: ignore # avoid isinstance checks for efficiency
+        """Get value for i-th solution of a given variable."""
+        if self.model is None:  # for mypy
+            raise RuntimeError(
+                "self.model should not be None when calling this method."
+            )
+        self.model.params.SolutionNumber = i
+        return var.getAttr("Xn")
+
+    def get_obj_value_for_ith_solution(self, i: int):
+        """Get objective value for i-th solution."""
+        if self.model is None:  # for mypy
+            raise RuntimeError(
+                "self.model should not be None when calling this method."
+            )
+        self.model.params.SolutionNumber = i
+        return self.model.getAttr("ObjVal")
+
+    def get_pool_obj_value_for_ith_solution(self, i: int):
+        """Get pool objective value for i-th solution."""
+        if self.model is None:  # for mypy
+            raise RuntimeError(
+                "self.model should not be None when calling this method."
+            )
+        self.model.params.SolutionNumber = i
+        return self.model.getAttr("PoolObjVal")
+
+    @property
+    def nb_solutions(self) -> int:
+        """Number of solutions found by the solver."""
+        if self.model is None:
+            return 0
+        else:
+            return self.model.SolCount
