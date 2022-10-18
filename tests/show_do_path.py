35bbbcc5a1b986fa8bd8c6bb8f2abbbe2ca7eefd
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2022-10-18 12:34:28 +0200

    Show discrete_optimization path used by pytest

---
+++ tests/show_do_path.py
@@ -0,0 +1,9 @@
+#  Copyright (c) 2022 AIRBUS and its affiliates.
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+
+import discrete_optimization
+
+
+def test_show_do_path():
+    print(discrete_optimization.__file__)
