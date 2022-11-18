338a876528e5c9a3b8cdf283d4e0163c73e173eb
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2022-11-18 10:54:35 +0100

    Skip minizinc version check if proper env variable set
    To ensure the library can work, we check in discrete_optimization.__init__.py
    that minzinc binary is installed and configured with the correct version.
    
    But in some cases, users want to use only a part of the library not
    relying on minizinc. We allow this by skipping the check step
    if the environment variable DO_SKIP_MZN_CHECK is set and evaluates as a
    boolean to True.
    
    This can be done for instance in shell with
    
    	export DO_SKIP_MZN_CHECK=1

--- discrete_optimization/__init__.py
+++ discrete_optimization/__init__.py
@@ -2,25 +2,34 @@
 #  This source code is licensed under the MIT license found in the
 #  LICENSE file in the root directory of this source tree.
 
-import minizinc
+import os
 
 __version__ = "0.0.0"
 
-# Check that minimal minizinc binary version is respected
-_minizinc_minimal_parsed_version = (2, 6)
-_minizinc_minimal_str_version = ".".join(
-    str(i) for i in _minizinc_minimal_parsed_version
-)
 
-if minizinc.default_driver is None:
-    raise RuntimeError(
-        "Minizinc binary has not been found.\n"
-        "You need to install it and/or configure the PATH environment variable.\n"
-        "See minizinc documentation for more details: https://www.minizinc.org/doc-latest/en/installation.html"
+# Check that minimal minizinc binary version is respected,
+# except if environment variable DO_SKIP_MZN_CHECK is set to 1.
+if ("DO_SKIP_MZN_CHECK" not in os.environ) or not (os.environ["DO_SKIP_MZN_CHECK"]):
+    import minizinc
+
+    _minizinc_minimal_parsed_version = (2, 6)
+    _minizinc_minimal_str_version = ".".join(
+        str(i) for i in _minizinc_minimal_parsed_version
     )
-if minizinc.default_driver.parsed_version < _minizinc_minimal_parsed_version:
-    raise RuntimeError(
-        f"Minizinc binary version must be at least {_minizinc_minimal_str_version}.\n"
-        "Install an appropriate version of minizinc and/or configure the PATH environment variable.\n"
-        "See minizinc documentation for more details: https://www.minizinc.org/doc-latest/en/installation.html"
-    )
+
+    if minizinc.default_driver is None:
+        raise RuntimeError(
+            "Minizinc binary has not been found.\n"
+            "You need to install it and/or configure the PATH environment variable.\n"
+            "See minizinc documentation for more details: https://www.minizinc.org/doc-latest/en/installation.html\n\n"
+            "You can also bypass this check by setting the environment variable DO_SKIP_MZN_CHECK to 1, "
+            "at your own risk."
+        )
+    if minizinc.default_driver.parsed_version < _minizinc_minimal_parsed_version:
+        raise RuntimeError(
+            f"Minizinc binary version must be at least {_minizinc_minimal_str_version}.\n"
+            "Install an appropriate version of minizinc and/or configure the PATH environment variable.\n"
+            "See minizinc documentation for more details: https://www.minizinc.org/doc-latest/en/installation.html\n\n"
+            "You can also bypass this check by setting the environment variable DO_SKIP_MZN_CHECK to 1, "
+            "at your own risk."
+        )
