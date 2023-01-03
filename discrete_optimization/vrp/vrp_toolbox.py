6522dfb7e5445224f337637befc21f1cf32a5c82
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2023-01-03 15:29:08 +0100

    Remove unused vrp code

--- discrete_optimization/vrp/vrp_toolbox.py
+++ discrete_optimization/vrp/vrp_toolbox.py
@@ -13,8 +13,6 @@ from discrete_optimization.vrp.vrp_model import VrpProblem
 
 logger = logging.getLogger(__name__)
 
-Customer = namedtuple("Customer", ["index", "demand", "x", "y"])
-
 
 def compute_length_matrix(vrp_model: VrpProblem):
     nb_customers = vrp_model.customer_count
@@ -49,60 +47,3 @@ def build_graph(vrp_model: VrpProblem):
     G = nx.from_numpy_matrix(matrix_adjacency, create_using=nx.DiGraph)
     G.add_edge(0, 0, weight=0)
     return G, matrix_distance
-
-
-def length(customer1, customer2):
-    return math.sqrt(
-        (customer1.x - customer2.x) ** 2 + (customer1.y - customer2.y) ** 2
-    )
-
-
-def parse_input(input_data):
-    # parse the input
-    lines = input_data.split("\n")
-
-    parts = lines[0].split()
-    customer_count = int(parts[0])
-    vehicle_count = int(parts[1])
-    vehicle_capacity = int(parts[2])
-
-    customers = []
-    for i in range(1, customer_count + 1):
-        line = lines[i]
-        parts = line.split()
-        customers.append(
-            Customer(i - 1, int(parts[0]), float(parts[1]), float(parts[2]))
-        )
-    return vehicle_count, vehicle_capacity, customer_count, customers
-
-
-def recompute_output(dictionary_path, customers):
-    vehicle_tours = []
-    obj = 0
-    for k in dictionary_path:
-        path = dictionary_path[k] + [0]
-        range_index = range(len(path))
-        obj += sum(
-            [
-                length(customers[ii], customers[jj])
-                for ii, jj in zip(path[:-1], path[1:])
-            ]
-        )
-        vehicle_tours += [[customers[j] for j in path[1:-1]]]
-    return vehicle_tours, obj, 0
-
-
-def output_result(vehicle_tours, obj, opt, vehicle_count, depot):
-    # prepare the solution in the specified output format
-    outputData = f"{obj:.2f} {opt}\n"
-    logger.debug(len(vehicle_tours))
-    for v in range(0, vehicle_count):
-        outputData += (
-            str(depot.index)
-            + " "
-            + " ".join([str(customer.index) for customer in vehicle_tours[v]])
-            + " "
-            + str(depot.index)
-            + "\n"
-        )
-    return outputData
