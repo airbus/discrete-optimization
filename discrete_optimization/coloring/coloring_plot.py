dcc4d80928afdd4334dc21940ec71d093eae4768
poveda_g <guillaume.poveda@airbus.com>
2023-02-02 09:34:55 +0100

    adding plot function for coloring solution

---
+++ discrete_optimization/coloring/coloring_plot.py
@@ -0,0 +1,26 @@
+#  Copyright (c) 2023 AIRBUS and its affiliates.
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+from discrete_optimization.coloring.coloring_model import (
+    ColoringProblem,
+    ColoringSolution,
+)
+
+
+def plot_solution(solution: ColoringSolution):
+    problem: ColoringProblem = solution.problem
+    graph_nx = problem.graph.graph_nx
+    pos = nx.kamada_kawai_layout(graph_nx)
+    fig, ax = plt.subplots(1, figsize=(15, 15))
+    nx.draw_networkx_nodes(
+        graph_nx,
+        pos=pos,
+        nodelist=problem.graph.nodes_name,
+        node_color=[solution.colors[i] for i in range(len(problem.graph.nodes_name))],
+        ax=ax,
+    )
+    nx.draw_networkx_edges(graph_nx, pos=pos, ax=ax)
