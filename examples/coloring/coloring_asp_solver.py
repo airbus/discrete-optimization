bfc37de87d59eb80c12185e25a33e0d802d804b0
poveda_g <guillaume.poveda@airbus.com>
2023-02-02 09:39:53 +0100

    update examples of asp solving

--- examples/coloring/coloring_asp_solver.py
+++ examples/coloring/coloring_asp_solver.py
@@ -11,17 +11,20 @@ from discrete_optimization.coloring.coloring_parser import (
     get_data_available,
     parse_file,
 )
+from discrete_optimization.coloring.coloring_plot import plot_solution, plt
 from discrete_optimization.coloring.solvers.coloring_asp_solver import ColoringASPSolver
 
 
 def run_asp_coloring():
     logging.basicConfig(level=logging.DEBUG)
-    file = [f for f in get_data_available() if "gc_250_5" in f][0]
+    file = [f for f in get_data_available() if "gc_20_9" in f][0]
     color_problem = parse_file(file)
     solver = ColoringASPSolver(color_problem, params_objective_function=None)
-    solver.init_model(max_models=100, nb_colors=None)
+    solver.init_model(max_models=1, nb_colors=None)
     result_store = solver.solve(timeout_seconds=100, use_external_timeout=True)
     solution, fit = result_store.get_best_solution_fit()
+    plot_solution(solution)
+    plt.show()
     print(solution, fit)
     print("Evaluation : ", color_problem.evaluate(solution))
     print("Satisfy : ", color_problem.satisfy(solution))
