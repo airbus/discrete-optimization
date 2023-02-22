85b5273fb963cc60769b3faa040000ebb74da524
poveda_g <guillaume.poveda@airbus.com>
2023-02-22 11:45:16 +0100

    ASP solver as subclass of SolverKnapsack

--- discrete_optimization/knapsack/solvers/knapsack_asp_solver.py
+++ discrete_optimization/knapsack/solvers/knapsack_asp_solver.py
@@ -9,31 +9,28 @@ from typing import Any, List, Optional
 import clingo
 from clingo import Symbol
 
-
-from discrete_optimization.generic_tools.result_storage.result_storage import (
-    ResultStorage,
-)
-
-from discrete_optimization.knapsack.knapsack_model import (
-    KnapsackModel,
-    KnapsackSolution,
-)
-
-from discrete_optimization.knapsack.solvers.knapsack_solver import SolverKnapsack
-
 from discrete_optimization.generic_tools.do_problem import (
     ParamsObjectiveFunction,
     build_aggreg_function_and_params_objective,
 )
+from discrete_optimization.generic_tools.result_storage.result_storage import (
+    ResultStorage,
+)
+from discrete_optimization.knapsack.knapsack_model import (
+    KnapsackModel,
+    KnapsackSolution,
+)
+from discrete_optimization.knapsack.solvers.knapsack_solver import SolverKnapsack
 
 cur_folder = os.path.abspath(os.path.dirname(__file__))
 logger = logging.getLogger(__name__)
 
-class KnapsackASPSolver():
+
+class KnapsackASPSolver(SolverKnapsack):
     def __init__(
-            self,
-            knapsack_model: KnapsackModel,
-            params_objective_function: Optional[ParamsObjectiveFunction] = None,
+        self,
+        knapsack_model: KnapsackModel,
+        params_objective_function: Optional[ParamsObjectiveFunction] = None,
     ):
         SolverKnapsack.__init__(self, knapsack_model=knapsack_model)
         (
@@ -41,27 +38,23 @@ class KnapsackASPSolver():
             self.aggreg_from_dict_values,
             self.params_objective_function,
         ) = build_aggreg_function_and_params_objective(
-            self.knapsack_model,
-            params_objective_function=params_objective_function
+            self.knapsack_model, params_objective_function=params_objective_function
         )
         self.model: Optional[clingo.Control] = None
 
     def init_model(self, **kwargs: Any) -> None:
-        basic_model = """ 
+        basic_model = """
         % knapsack bound
         % choice rule: in/1 is a subset of the set of items.
         {in(I):weight(I,W)} :- weight(I,W).
-        
+
         % integrity constraint: total weight of items in this subset doesn't exceecd d max_weight.
         :- #sum{W,I: in(I), weight(I,W)} > max_weight.
-        
+
         % optimization: maximize the values for in/1.
-        #maximize {V,I : in(I), value(I,V)}.
-        
-        
+        #maximize {V,I : in(I), value(I,V)}.        
         %total_value(X) :- X = #sum{V,I: in(I), value(I,V)}.
         %total_weight(X) :- X = #sum{W,I: in(I), weight(I,W)}.
-        
         #show in/1.
         %#show total_value/1.
         %#show total_weight/1.
@@ -89,7 +82,10 @@ class KnapsackASPSolver():
         ]
         max_capacity = self.knapsack_model.max_capacity
 
-        items = [f"weight({i},{weights[i]}). value({i},{values[i]})." for i in range(1,len(weights))]
+        items = [
+            f"weight({i},{weights[i]}). value({i},{values[i]})."
+            for i in range(1, len(weights))
+        ]
 
         print(f"#const max_weight={max_capacity}." + " ".join(items))
         return f"#const max_weight={max_capacity}." + " ".join(items)
@@ -97,9 +93,14 @@ class KnapsackASPSolver():
     def retrieve_solutions(self, list_symbols: List[List[Symbol]]) -> ResultStorage:
         list_solutions_fit = []
         for symbols in list_symbols:
-            in_list = [s.arguments[0].number for s in symbols if s.name=="in"]
-            list_taken = [1 if i in in_list else 0 for i in range(1,self.knapsack_model.nb_items+1)]
-            solution = KnapsackSolution(problem=self.knapsack_model, list_taken=list_taken)
+            in_list = [s.arguments[0].number for s in symbols if s.name == "in"]
+            list_taken = [
+                1 if i in in_list else 0
+                for i in range(1, self.knapsack_model.nb_items + 1)
+            ]
+            solution = KnapsackSolution(
+                problem=self.knapsack_model, list_taken=list_taken
+            )
             fit = self.aggreg_sol(solution)
             list_solutions_fit += [(solution, fit)]
 
