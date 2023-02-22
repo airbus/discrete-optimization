7a7837dcfd4870b0c7f5e5abf585d83742dd06a6
eadietz <emmanuelle.dietz@airbus.com>
2023-02-22 08:55:43 +0100

    knapsack for ASP

---
+++ examples/knapsack/knapsack_asp_solver.py
@@ -0,0 +1,34 @@
+#  Copyright (c) 2022 AIRBUS and its affiliates.
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+
+import logging
+import os
+
+os.environ["DO_SKIP_MZN_CHECK"] = "1"
+
+from discrete_optimization.knapsack.knapsack_model import (
+    KnapsackModel,
+    KnapsackSolution,
+)
+from discrete_optimization.knapsack.knapsack_parser import (
+    get_data_available,
+    parse_file,
+)
+
+from discrete_optimization.knapsack.solvers.knapsack_asp_solver import KnapsackASPSolver
+
+def run_asp_coloring():
+    logging.basicConfig(level=logging.INFO)
+    file = [f for f in get_data_available() if "ks_4_0" in f][0]
+    knapsack_model = parse_file(file)
+    solver = KnapsackASPSolver(knapsack_model, params_objective_function=None)
+    solver.init_model(max_models=50, nb_colors=20)
+    result_store = solver.solve(timeout_seconds=50000)
+    solution, fit = result_store.get_best_solution_fit()
+    #plot_solution(solution)
+    #plt.show()
+    print(solution, fit)
+
+if __name__ == "__main__":
+    run_asp_coloring()
