f3a58bba211a34cad46da32507dc356b1a1c7346
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2024-03-26 16:08:26 +0100

    Add docstrings in hyperparameter.py and explicit parts of **kwargs

--- discrete_optimization/generic_tools/hyperparameters/hyperparameter.py
+++ discrete_optimization/generic_tools/hyperparameters/hyperparameter.py
@@ -6,69 +6,205 @@ from __future__ import annotations  # see annotations as str
 
 from dataclasses import dataclass, field
 from enum import Enum
-from typing import TYPE_CHECKING, Any, List, Optional
+from typing import TYPE_CHECKING, Any, Dict, Iterable, List, Optional, Type
 
 if TYPE_CHECKING:  # only for type checkers
     try:
-        import optuna
+        import optuna  # not necessary to import this module
     except ImportError:
         pass
 
 
 @dataclass
 class Hyperparameter:
+    """Hyperparameter base class used to specify d-o solver hyperparameters."""
+
     name: str
+    """Name of the hyperparameter.
+
+    Should correspond to how the hyperparameter is specified in the solver
+    `__init__()`, `init_model()`, or `solve()` keywords arguments.
+
+    """
+
     default: Optional[Any] = None
+    """Default value for the hyperparameter.
+
+    None means "no default value".
+
+    """
 
     def suggest_with_optuna(self, trial: optuna.trial.Trial, **kwargs: Any) -> Any:
+        """Suggest hyperparameter value for an Optuna trial.
+
+        Args:
+            trial: optuna Trial used for choosing the hyperparameter value
+            **kwargs: passed to `trial.suggest_xxx()`
+
+        Returns:
+
+        """
         ...
 
 
 @dataclass
 class IntegerHyperparameter(Hyperparameter):
-    low: Optional[int] = None
-    high: Optional[int] = None
-    default: Optional[int] = None
+    """Integer hyperparameter."""
 
-    def suggest_with_optuna(self, trial: optuna.trial.Trial, **kwargs: Any) -> Any:
-        if self.low is not None and "low" not in kwargs:
-            kwargs["low"] = self.low
-        if self.high is not None and "high" not in kwargs:
-            kwargs["high"] = self.high
-        return trial.suggest_int(name=self.name, **kwargs)
+    low: Optional[int] = None
+    """Lower bound.
+
+    If None, the hyperparameter value has no lower bound.
+
+    """
+
+    high: Optional[int] = None
+    """Upper bound.
+
+    If None, the hyperparameter value has no upper bound.
+
+    """
+
+    default: Optional[int] = None
+    """Default value for the hyperparameter.
+
+    None means "no default value".
+
+    """
+
+    def suggest_with_optuna(
+        self,
+        trial: optuna.trial.Trial,
+        low: Optional[int] = None,
+        high: Optional[int] = None,
+        **kwargs: Any,
+    ) -> Any:
+        """Suggest hyperparameter value for an Optuna trial.
+
+        Args:
+            trial: optuna Trial used for choosing the hyperparameter value
+            low: can be used to restrict lower bound
+            high: can be used to restrict upper bound
+            **kwargs: passed to `trial.suggest_int()`
+
+        Returns:
+
+        """
+        if low is None:
+            low = self.low
+        if high is None:
+            high = self.high
+        return trial.suggest_int(name=self.name, low=low, high=high, **kwargs)  # type: ignore
 
 
 @dataclass
 class FloatHyperparameter(Hyperparameter):
-    low: Optional[float] = None
-    high: Optional[float] = None
-    default: Optional[float] = None
+    """Float parameter."""
 
-    def suggest_with_optuna(self, trial: optuna.trial.Trial, **kwargs: Any) -> Any:
-        if self.low is not None and "low" not in kwargs:
-            kwargs["low"] = self.low
-        if self.high is not None and "high" not in kwargs:
-            kwargs["high"] = self.high
-        return trial.suggest_float(name=self.name, **kwargs)
+    low: Optional[float] = None
+    """Lower bound.
+
+    If None, the hyperparameter value has no lower bound.
+
+    """
+
+    high: Optional[float] = None
+    """Upper bound.
+
+    If None, the hyperparameter value has no upper bound.
+
+    """
+
+    default: Optional[float] = None
+    """Default value for the hyperparameter.
+
+    None means "no default value".
+
+    """
+
+    def suggest_with_optuna(
+        self,
+        trial: optuna.trial.Trial,
+        low: Optional[float] = None,
+        high: Optional[float] = None,
+        **kwargs: Any,
+    ) -> Any:
+        """Suggest hyperparameter value for an Optuna trial.
+
+        Args:
+            trial: optuna Trial used for choosing the hyperparameter value
+            low: can be used to restrict lower bound
+            high: can be used to restrict upper bound
+            **kwargs: passed to `trial.suggest_float()`
+
+        Returns:
+
+        """
+        if low is None:
+            low = self.low
+        if high is None:
+            high = self.high
+        return trial.suggest_float(name=self.name, low=low, high=high, **kwargs)
 
 
 @dataclass
 class CategoricalHyperparameter(Hyperparameter):
-    choices: List[Any] = field(default_factory=list)
+    """Categorical hyperparameter."""
 
-    def suggest_with_optuna(self, trial: optuna.trial.Trial, **kwargs: Any) -> Any:
+    choices: List[Any] = field(default_factory=list)
+    """List of possible choices."""
+
+    def suggest_with_optuna(
+        self,
+        trial: optuna.trial.Trial,
+        choices: Optional[Iterable[Any]] = None,
+        **kwargs: Any,
+    ) -> Any:
+        """Suggest hyperparameter value for an Optuna trial.
+
+        Args:
+            trial: optuna Trial used for choosing the hyperparameter value
+            choices: restricts list of choices
+            **kwargs: passed to `trial.suggest_categorical()`
+
+        Returns:
+
+        """
         if self.choices is not None and "choices" not in kwargs:
             kwargs["choices"] = self.choices
         return trial.suggest_categorical(name=self.name, **kwargs)
 
 
 class EnumHyperparameter(CategoricalHyperparameter):
-    def __init__(self, name: str, enum: Enum, default: Optional[Any] = None):
+    """Hyperparameter taking value among an enumeration.
+
+    Args:
+        enum: enumeration used to create the hyperparameter
+
+    """
+
+    def __init__(self, name: str, enum: Type[Enum], default: Optional[Any] = None):
         super().__init__(name, choices=list(enum), default=default)
         self.enum = enum
 
-    def suggest_with_optuna(self, trial: optuna.trial.Trial, **kwargs: Any) -> Any:
-        choices = kwargs.get("choices", self.choices)
+    def suggest_with_optuna(
+        self,
+        trial: optuna.trial.Trial,
+        choices: Optional[Iterable[Enum]] = None,
+        **kwargs: Any,
+    ) -> Enum:
+        """Suggest hyperparameter value for an Optuna trial.
+
+        Args:
+            trial: optuna Trial used for choosing the hyperparameter value
+            choices: restricts list of choices among the enumeration `self.enum`
+            **kwargs: passed to `trial.suggest_categorical()`
+
+        Returns:
+
+        """
+        if choices is None:
+            choices = self.choices
         choices_str = [c.name for c in choices]
-        choice_str = trial.suggest_categorical(name=self.name, choices=choices_str)
+        choice_str: str = trial.suggest_categorical(name=self.name, choices=choices_str, **kwargs)  # type: ignore
         return self.enum[choice_str]
