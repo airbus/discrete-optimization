2f330535c02b792610f784eb2b8790a7622066c8
poveda_g <guillaume.poveda@airbus.com>
2024-04-12 14:08:49 +0200

    Plot utils for gpdp problem
    the plot_ortools_solution is generic and is moved to gpdp_plot_utils instead

---
+++ discrete_optimization/pickup_vrp/plots/gpdp_plot_utils.py
@@ -0,0 +1,50 @@
+#  Copyright (c) 2024 AIRBUS and its affiliates.
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+from __future__ import print_function
+
+from typing import Tuple
+
+import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+
+from discrete_optimization.pickup_vrp.gpdp import GPDP, GPDPSolution
+
+
+def plot_gpdp_solution(
+    sol: GPDPSolution,
+    problem: GPDP,
+) -> Tuple[Figure, Axes]:
+    if problem.coordinates_2d is None:
+        raise ValueError(
+            "problem.coordinates_2d cannot be None when calling plot_ortools_solution."
+        )
+    vehicle_tours = sol.trajectories
+    fig, ax = plt.subplots(1)
+    nb_colors = problem.number_vehicle
+    nb_colors_clusters = len(problem.clusters_set)
+    colors_nodes = plt.cm.get_cmap("hsv", nb_colors_clusters)
+    ax.scatter(
+        [problem.coordinates_2d[node][0] for node in problem.clusters_dict],
+        [problem.coordinates_2d[node][1] for node in problem.clusters_dict],
+        s=1,
+        color=[
+            colors_nodes(problem.clusters_dict[node]) for node in problem.clusters_dict
+        ],
+    )
+    for v, traj in vehicle_tours.items():
+        ax.plot(
+            [problem.coordinates_2d[node][0] for node in traj],
+            [problem.coordinates_2d[node][1] for node in traj],
+            label="vehicle n°" + str(v),
+        )
+        ax.scatter(
+            [problem.coordinates_2d[node][0] for node in traj],
+            [problem.coordinates_2d[node][1] for node in traj],
+            s=10,
+            color=[colors_nodes(problem.clusters_dict[node]) for node in traj],
+        )
+    ax.legend()
+    return fig, ax
