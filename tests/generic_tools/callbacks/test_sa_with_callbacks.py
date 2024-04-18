25bd552e63e2b9af4ae3cb0f81e95ba7b6ea8ae0
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2024-04-18 16:24:31 +0200

    Add test for timed optuna callback and timed pruner

--- tests/generic_tools/callbacks/test_sa_with_callbacks.py
+++ tests/generic_tools/callbacks/test_sa_with_callbacks.py
@@ -37,6 +37,9 @@ from discrete_optimization.generic_tools.mutations.mutation_catalog import (
     PermutationMutationRCPSP,
     get_available_mutations,
 )
+from discrete_optimization.generic_tools.optuna.timed_percentile_pruner import (
+    TimedPercentilePruner,
+)
 from discrete_optimization.rcpsp.rcpsp_model import RCPSPModel
 from discrete_optimization.rcpsp.rcpsp_parser import get_data_available, parse_file
 
@@ -198,6 +201,99 @@ def test_sa_with_optuna_callback(random_seed):
     )
     study.set_metric_names(["-makespan"])
     study.optimize(objective, n_trials=10)
+
+    pruned_trials = study.get_trials(
+        deepcopy=False, states=[optuna.trial.TrialState.PRUNED]
+    )
+    complete_trials = study.get_trials(
+        deepcopy=False, states=[optuna.trial.TrialState.COMPLETE]
+    )
+    assert len(pruned_trials) > 0
+    assert len(complete_trials) > 0
+
+
+def test_sa_with_optuna_timed_callback(random_seed):
+    def objective(trial: optuna.trial.Trial) -> float:
+        files = get_data_available()
+        files = [f for f in files if "j1010_1.mm" in f]  # Multi mode RCPSP
+        file_path = files[0]
+        rcpsp_model: RCPSPModel = parse_file(file_path)
+        rcpsp_model.set_fixed_modes([1 for i in range(rcpsp_model.n_jobs)])
+
+        dummy = rcpsp_model.get_dummy_solution()
+        _, mutations = get_available_mutations(rcpsp_model, dummy)
+        list_mutation = [
+            mutate[0].build(rcpsp_model, dummy, **mutate[1])
+            for mutate in mutations
+            if mutate[0] == PermutationMutationRCPSP
+        ]
+        mixed_mutation = BasicPortfolioMutation(
+            list_mutation, np.ones((len(list_mutation)))
+        )
+        objectives = ["makespan"]
+        objective_weights = [-1]
+        params_objective_function = ParamsObjectiveFunction(
+            objective_handling=ObjectiveHandling.AGGREGATE,
+            objectives=objectives,
+            weights=objective_weights,
+            sense_function=ModeOptim.MAXIMIZATION,
+        )
+
+        initial_temperature = trial.suggest_float(
+            "initial_temperature", 0.1, 100.0, log=True
+        )
+        one_minus_coefficient_temperature = trial.suggest_float(
+            "one_minus_coefficient_temperature", 0.00001, 0.2, log=True
+        )
+        coefficient_temperature = 1.0 - one_minus_coefficient_temperature
+        nb_iteration_max = trial.suggest_int("nb_iteration_max", 100, 1000)
+        nb_iteration_no_improvement_max = trial.suggest_int(
+            "nb_iteration_no_improvement_max", 10, nb_iteration_max
+        )
+
+        restart_handler = RestartHandlerLimit(
+            nb_iteration_no_improvement=nb_iteration_no_improvement_max
+        )
+        temperature_handler = TemperatureSchedulingFactor(
+            temperature=initial_temperature,
+            restart_handler=restart_handler,
+            coefficient=coefficient_temperature,
+        )
+
+        sa = SimulatedAnnealing(
+            problem=rcpsp_model,
+            mutator=mixed_mutation,
+            restart_handler=restart_handler,
+            temperature_handler=temperature_handler,
+            mode_mutation=ModeMutation.MUTATE,
+            params_objective_function=params_objective_function,
+            store_solution=False,
+        )
+
+        _, fit = sa.solve(
+            dummy,
+            nb_iteration_max=nb_iteration_max,
+            callbacks=[
+                OptunaCallback(
+                    trial=trial,
+                    optuna_report_nb_steps=10,
+                    report_time=True,
+                )
+            ],
+        ).get_best_solution_fit()
+        return fit
+
+    study = optuna.create_study(
+        study_name="rcpsp-sa-pruning",
+        direction="maximize",
+        sampler=optuna.samplers.TPESampler(seed=random_seed),
+        pruner=TimedPercentilePruner(  # intermediate values interpolated at same "step"
+            percentile=2,  # 2% pruner
+            n_min_trials=2,
+        ),
+    )
+    study.set_metric_names(["-makespan"])
+    study.optimize(objective, n_trials=40)
 
     pruned_trials = study.get_trials(
         deepcopy=False, states=[optuna.trial.TrialState.PRUNED]
