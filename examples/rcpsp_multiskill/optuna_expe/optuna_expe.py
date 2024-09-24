b50950ccc4baed14247b2e4fd610380885d9add3
poveda_g <guillaume.poveda@airbus.com>
2024-09-24 20:48:03 +0200

    adding optuna script for multiskill rcpsp

---
+++ examples/rcpsp_multiskill/optuna_expe/optuna_expe.py
@@ -0,0 +1,34 @@
+#  Copyright (c) 2024 AIRBUS and its affiliates.
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+
+from discrete_optimization.generic_tools.optuna.utils import (
+    generic_optuna_experiment_monoproblem,
+)
+from discrete_optimization.rcpsp_multiskill.rcpsp_multiskill_mslib_parser import (
+    get_data_available,
+    parse_file_mslib,
+)
+from discrete_optimization.rcpsp_multiskill.solvers.cp_solvers import CP_MS_MRCPSP_MZN
+from discrete_optimization.rcpsp_multiskill.solvers.cpsat_msrcpsp_solver import (
+    CPSatMSRCPSPSolver,
+)
+
+
+def script_optuna():
+    files_dict = get_data_available()
+    file = [f for f in files_dict["MSLIB4"] if "MSLIB_Set4_1003.msrcp" in f][0]
+    problem = parse_file_mslib(file, skill_level_version=False)
+    generic_optuna_experiment_monoproblem(
+        problem=problem,
+        solvers_to_test=[CPSatMSRCPSPSolver, CP_MS_MRCPSP_MZN],
+        kwargs_fixed_by_solver={
+            CPSatMSRCPSPSolver: {"time_limit": 20},
+            CP_MS_MRCPSP_MZN: {"time_limit": 20},
+        },
+        n_trials=100,
+    )
+
+
+if __name__ == "__main__":
+    script_optuna()
