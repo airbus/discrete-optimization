1f2fbab2c6797b1a2d015b80d1035636abac125c
poveda_g <guillaume.poveda@airbus.com>
2024-09-24 11:07:38 +0200

    adding redundant constraint for msrcpsp

--- discrete_optimization/rcpsp_multiskill/solvers/cpsat_msrcpsp_solver.py
+++ discrete_optimization/rcpsp_multiskill/solvers/cpsat_msrcpsp_solver.py
@@ -2,8 +2,10 @@
 #  This source code is licensed under the MIT license found in the
 #  LICENSE file in the root directory of this source tree.
 import logging
+import math
 from typing import Any, Iterable, List
 
+import numpy as np
 from ortools.sat.python.cp_model import (
     Constraint,
     CpModel,
@@ -13,6 +15,9 @@ from ortools.sat.python.cp_model import (
 )
 
 from discrete_optimization.generic_tools.do_problem import Problem, Solution
+from discrete_optimization.generic_tools.hyperparameters.hyperparameter import (
+    CategoricalHyperparameter,
+)
 from discrete_optimization.generic_tools.ortools_cpsat_tools import OrtoolsCPSatSolver
 from discrete_optimization.generic_tools.result_storage.result_storage import (
     ResultStorage,
@@ -20,13 +25,23 @@ from discrete_optimization.generic_tools.result_storage.result_storage import (
 from discrete_optimization.rcpsp_multiskill.rcpsp_multiskill import (
     MS_RCPSPModel,
     MS_RCPSPSolution,
+    compute_discretize_calendar_skills,
     create_fake_tasks_multiskills,
+    discretize_calendar_,
 )
 
 logger = logging.getLogger(__name__)
 
 
 class CPSatMSRCPSPSolver(OrtoolsCPSatSolver):
+    hyperparameters = [
+        CategoricalHyperparameter(
+            name="redundant_skill_cumulative", choices=[True, False], default=True
+        ),
+        CategoricalHyperparameter(
+            name="redundant_worker_cumulative", choices=[True, False], default=True
+        ),
+    ]
     problem: MS_RCPSPModel
 
     def __init__(self, problem: Problem, **kwargs: Any):
@@ -52,9 +67,11 @@ class CPSatMSRCPSPSolver(OrtoolsCPSatSolver):
         return min(s._intern_obj[obj] for s, _ in res.list_solution_fits)
 
     def init_model(self, **args: Any) -> None:
+        args = self.complete_with_default_hyperparameters(args)
         one_worker_per_task = args.get("one_worker_per_task", False)
         one_skill_per_task = args.get("one_skill_per_task", False)
-
+        redundant_skill_cumulative = args["redundant_skill_cumulative"]
+        redundant_worker_cumulative = args["redundant_worker_cumulative"]
         self.cp_model = CpModel()
         self.variables = {}
         self.create_base_variable()
@@ -68,6 +85,10 @@ class CPSatMSRCPSPSolver(OrtoolsCPSatSolver):
             self.problem
         )
         self.create_constraint_resource()
+        if redundant_skill_cumulative:
+            self.constraint_redundant_cumulative_skills()
+        if redundant_worker_cumulative:
+            self.constraint_redundant_cumulative_worker()
         self.create_disjunctive_worker()
         self.create_skills_constraint_to_mode()
         self.create_skills_constraint_worker(**args)
@@ -523,6 +544,125 @@ class CPSatMSRCPSPSolver(OrtoolsCPSatSolver):
                 [x[1] for x in intervals_consume] + [x[1] for x in calendar_tasks],
                 capacity=1,
             )
+
+    def constraint_redundant_cumulative_skills(self):
+        discr_calendar, dict_calendar_skills = compute_discretize_calendar_skills(
+            problem=self.problem
+        )
+        for skill in self.problem.skills_set:
+            intervals_consume = []
+            for task in self.problem.tasks_list:
+                modes = list(self.problem.mode_details[task].keys())
+                if len(modes) == 1:
+                    if self.problem.mode_details[task][modes[0]].get(skill, 0) > 0:
+                        intervals_consume.append(
+                            (
+                                self.variables["base_variable"]["intervals"][task],
+                                self.problem.mode_details[task][modes[0]][skill],
+                            )
+                        )
+                else:
+                    for mode in modes:
+                        if self.problem.mode_details[task][mode].get(skill, 0) > 0:
+                            intervals_consume.append(
+                                (
+                                    self.variables["mode_variable"]["opt_intervals"][
+                                        task
+                                    ][mode],
+                                    self.problem.mode_details[task][mode][skill],
+                                )
+                            )
+            calendar_tasks = [
+                (
+                    self.cp_model.NewFixedSizedIntervalVar(
+                        start=f["start"], size=f["duration"], name="calendar_res"
+                    ),
+                    f.get("value", 0),
+                )
+                for f in discr_calendar[skill]
+                if f.get("value", 0) > 0
+            ]
+            self.cp_model.AddCumulative(
+                [x[0] for x in intervals_consume] + [x[0] for x in calendar_tasks],
+                [x[1] for x in intervals_consume] + [x[1] for x in calendar_tasks],
+                capacity=int(np.max(dict_calendar_skills[skill])),
+            )
+
+    def constraint_redundant_cumulative_worker(self):
+        merged_calendar = np.zeros(self.problem.horizon + 1)
+        for emp in self.problem.employees:
+            merged_calendar += np.array(self.problem.employees[emp].calendar_employee)
+        discr_calendar = discretize_calendar_(merged_calendar)
+        intervals_consume = []
+        max_skill_over_worker = {s: 0 for s in self.problem.skills_set}
+        for emp in self.problem.employees:
+            for s in self.problem.skills_set:
+                max_skill_over_worker[s] = max(
+                    max_skill_over_worker[s],
+                    self.problem.employees[emp].get_skill_level(s),
+                )
+        for task in self.problem.tasks_list:
+            modes = list(self.problem.mode_details[task].keys())
+            if len(modes) == 1:
+                skills_needed = {
+                    s: self.problem.mode_details[task][modes[0]].get(s, 0)
+                    for s in self.problem.skills_set
+                    if self.problem.mode_details[task][modes[0]].get(s, 0) > 0
+                }
+                if len(skills_needed) > 0:
+                    lb_nb_worker_needed = max(
+                        [
+                            int(math.ceil(skills_needed[s] / max_skill_over_worker[s]))
+                            for s in skills_needed
+                        ]
+                    )
+                    intervals_consume.append(
+                        (
+                            self.variables["base_variable"]["intervals"][task],
+                            lb_nb_worker_needed,
+                        )
+                    )
+            else:
+                for mode in modes:
+                    skills_needed = {
+                        s: self.problem.mode_details[task][modes[0]].get(s, 0)
+                        for s in self.problem.skills_set
+                        if self.problem.mode_details[task][modes[0]].get(s, 0) > 0
+                    }
+                    if len(skills_needed) > 0:
+                        lb_nb_worker_needed = max(
+                            [
+                                int(
+                                    math.ceil(
+                                        skills_needed[s] / max_skill_over_worker[s]
+                                    )
+                                )
+                                for s in skills_needed
+                            ]
+                        )
+                        intervals_consume.append(
+                            (
+                                self.variables["mode_variable"]["opt_intervals"][task][
+                                    mode
+                                ],
+                                lb_nb_worker_needed,
+                            )
+                        )
+        calendar_tasks = [
+            (
+                self.cp_model.NewFixedSizedIntervalVar(
+                    start=f["start"], size=f["duration"], name="calendar_res"
+                ),
+                f.get("value", 0),
+            )
+            for f in discr_calendar
+            if f.get("value", 0) > 0
+        ]
+        self.cp_model.AddCumulative(
+            [x[0] for x in intervals_consume] + [x[0] for x in calendar_tasks],
+            [x[1] for x in intervals_consume] + [x[1] for x in calendar_tasks],
+            capacity=self.problem.nb_employees,
+        )
 
     def constraint_mode(self):
         for task in self.variables["mode_variable"]["is_present"]:
