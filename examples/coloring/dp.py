60b986cb01394f74b03f22aa7ae7546d58b8998e
poveda_g <guillaume.poveda@airbus.com>
2024-10-11 19:56:41 +0200

    adding warmstart for did coloring solver, needed a new utils function in coloring problem for that

--- examples/coloring/dp.py
+++ examples/coloring/dp.py
@@ -4,11 +4,20 @@
 import logging
 
 from discrete_optimization.coloring.parser import get_data_available, parse_file
-from discrete_optimization.coloring.plot import plot_coloring_solution, plt
+from discrete_optimization.coloring.problem import (
+    transform_color_values_to_value_precede_on_other_node_order,
+)
 from discrete_optimization.coloring.solvers.dp import (
     DpColoringModeling,
     DpColoringSolver,
     dp,
+)
+from discrete_optimization.coloring.solvers.greedy import (
+    GreedyColoringSolver,
+    NxGreedyColoringMethod,
+)
+from discrete_optimization.generic_tools.callbacks.early_stoppers import (
+    NbIterationStopper,
 )
 
 
@@ -27,5 +36,40 @@ def run_dp_coloring():
     # plt.show()
 
 
+def run_did_coloring_ws():
+    logging.basicConfig(level=logging.INFO)
+    do_ws = True
+    file = [f for f in get_data_available() if "gc_500_7" in f][0]
+    color_problem = parse_file(file)
+    greedy = GreedyColoringSolver(color_problem)
+    sol, _ = greedy.solve(
+        strategy=NxGreedyColoringMethod.dsatur
+    ).get_best_solution_fit()
+    solver = DpColoringSolver(color_problem)
+    solver.init_model(modeling=DpColoringModeling.COLOR_TRANSITION, nb_colors=120)
+    if do_ws:
+        solver.set_warm_start(sol)
+    result_store = solver.solve(
+        solver=dp.LNBS,
+        callbacks=[NbIterationStopper(nb_iteration_max=20)],
+        threads=5,
+        use_callback=True,
+        time_limit=100,
+    )
+    solution, fit = result_store.get_best_solution_fit()
+    trans_ws = transform_color_values_to_value_precede_on_other_node_order(
+        sol.colors, nodes_ordering=solver.nodes_reordering
+    )
+    if do_ws:
+        print(trans_ws, solution.colors)
+        assert result_store[0][0].colors == trans_ws
+    print(solution, fit)
+    print("Evaluation : ", color_problem.evaluate(solution))
+    print("Satisfy : ", color_problem.satisfy(solution))
+    # plot_coloring_solution(solution)
+    # plt.show()
+
+
 if __name__ == "__main__":
     run_dp_coloring()
+    run_did_coloring_ws()
