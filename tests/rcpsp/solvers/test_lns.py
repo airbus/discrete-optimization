6c6fba36bfe74256ed74b266f54561e8f61a4848
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2024-10-17 14:03:27 +0200

    Fix lns init phase + constraint handler mix for minizinc subsolvers
    - Fix ConstraintHandlerMix for minizinc subsolvers (and add test)
    - Raise errors when lns has no initial_solution_provider but does not
      skip the initial phase or when it does not return a solution

--- tests/rcpsp/solvers/test_lns.py
+++ tests/rcpsp/solvers/test_lns.py
@@ -6,10 +6,15 @@ import pytest
 from discrete_optimization.generic_rcpsp_tools.solvers.lns_cp import (
     LnsCpMznGenericRcpspSolver,
 )
+from discrete_optimization.generic_rcpsp_tools.solvers.lns_cp.neighbor_builder import (
+    mix,
+)
 from discrete_optimization.generic_tools.callbacks.early_stoppers import TimerStopper
 from discrete_optimization.generic_tools.cp_tools import ParametersCp
+from discrete_optimization.generic_tools.lns_cp import LnsCpMzn
 from discrete_optimization.rcpsp.parser import get_data_available, parse_file
 from discrete_optimization.rcpsp.problem import RcpspProblem
+from discrete_optimization.rcpsp.solvers.cp_mzn import CpRcpspSolver
 
 
 @pytest.mark.parametrize(
@@ -33,4 +38,27 @@ def test_lns_solver(file_name):
         callbacks=[TimerStopper(total_seconds=20)],
     )
     sol, fit = results.get_best_solution_fit()
+    assert rcpsp_problem.satisfy(sol)
+
+
+@pytest.mark.parametrize(
+    "file_name",
+    ["j1201_1.sm"],
+)
+def test_mix_constraints_handlers(file_name):
+    files_available = get_data_available()
+    file = [f for f in files_available if file_name in f][0]
+    rcpsp_problem: RcpspProblem = parse_file(file)
+
+    constraints_handler = mix(rcpsp_problem=rcpsp_problem)
+    solver = LnsCpMzn(
+        problem=rcpsp_problem,
+        subsolver=CpRcpspSolver(problem=rcpsp_problem),
+        constraint_handler=constraints_handler,
+    )
+    solver.init_model()
+    res = solver.solve(
+        nb_iteration_lns=5, skip_initial_solution_provider=True, time_limit_subsolver=5
+    )
+    sol, fit = res.get_best_solution_fit()
     assert rcpsp_problem.satisfy(sol)
