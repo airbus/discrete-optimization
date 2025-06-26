00b3d3c0791c39985e7aaf04d4d16f8b320eacea
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2025-06-26 14:28:09 +0200

    Fix use of kwargs in CpmpySolver.solve()

--- discrete_optimization/generic_tools/cpmpy_tools.py
+++ discrete_optimization/generic_tools/cpmpy_tools.py
@@ -114,8 +114,9 @@ class CpmpySolver(CpSolver):
         callbacks_list = CallbackList(callbacks=callbacks)
         callbacks_list.on_solve_start(solver=self)
 
-        solver = kwargs.get("solver", "ortools")
-        self.model.solve(solver, time_limit=time_limit, **kwargs)
+        kwargs_solve = dict(kwargs)
+        solver = kwargs_solve.pop("solver", "ortools")
+        self.model.solve(solver, time_limit=time_limit, **kwargs_solve)
         self.cpm_status = self.model.cpm_status
         self.status_solver = map_exitstatus2statussolver[self.cpm_status.exitstatus]
 
