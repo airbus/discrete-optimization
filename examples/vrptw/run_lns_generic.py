8982df31e229d20ad9a53adace6f9ed508c21395
poveda_g <guillaume.poveda@airbus.com>
2025-10-31 14:39:17 +0100

    simplify cpsat model, update warmstart and the lns example

---
+++ examples/vrptw/run_lns_generic.py
@@ -0,0 +1,163 @@
+#  Copyright (c) 2025 AIRBUS and its affiliates.
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+import logging
+import random
+from typing import Any, Optional
+
+from discrete_optimization.generic_tasks_tools.base import TasksSolution
+from discrete_optimization.generic_tasks_tools.solvers.lns_cp.constraint_extractor import (
+    BaseConstraintExtractor,
+    ChainingConstraintExtractor,
+    ConstraintExtractorList,
+    ConstraintExtractorPortfolio,
+    DummyConstraintExtractor,
+    NbChangesAllocationConstraintExtractor,
+    NbUsagesAllocationConstraintExtractor,
+    SchedulingConstraintExtractor,
+    SubresourcesAllocationConstraintExtractor,
+    SubtasksAllocationConstraintExtractor,
+)
+from discrete_optimization.generic_tasks_tools.solvers.lns_cp.constraint_handler import (
+    ObjectiveSubproblem,
+    TasksConstraintHandler,
+)
+from discrete_optimization.generic_tasks_tools.solvers.lns_cp.neighbor_tools import (
+    NeighborBuilder,
+    NeighborBuilderMix,
+    NeighborBuilderSubPart,
+    NeighborRandom,
+)
+from discrete_optimization.generic_tools.callbacks.callback import Callback
+from discrete_optimization.generic_tools.callbacks.warm_start_callback import (
+    WarmStartCallback,
+    WarmStartCallbackLastRun,
+)
+from discrete_optimization.generic_tools.cp_tools import ParametersCp
+from discrete_optimization.generic_tools.do_problem import Solution
+from discrete_optimization.generic_tools.do_solver import SolverDO
+from discrete_optimization.generic_tools.lns_cp import LnsOrtoolsCpSat
+from discrete_optimization.generic_tools.result_storage.result_storage import (
+    ResultStorage,
+)
+from discrete_optimization.vrptw.parser import get_data_available, parse_vrptw_file
+from discrete_optimization.vrptw.solvers.cpsat import (
+    CpSatVRPTWSolver,
+    Task,
+    VRPTWProblem,
+    VRPTWSolution,
+)
+
+TIME_LIMIT_SUBSOLVER = 5
+logging.basicConfig(level=logging.INFO)
+
+
+class ReinitModel(Callback):
+    def on_step_end(
+        self, step: int, res: ResultStorage, solver: SolverDO
+    ) -> Optional[bool]:
+        pass
+        # pass
+        # solver.subsolver.init_model(scaling=1, cost_per_vehicle=1000)
+
+
+class NeighborBuilderVrpTw(NeighborBuilder[Task]):
+    def __init__(self, problem: VRPTWProblem, fraction_vehicle: float):
+        self.problem = problem
+        self.fraction_vehicle = fraction_vehicle
+
+    def find_subtasks(
+        self,
+        current_solution: TasksSolution[Task],
+        subtasks: Optional[set[Task]] = None,
+    ) -> tuple[set[Task], set[Task]]:
+        solution: VRPTWSolution = current_solution
+        vehicle_used = [
+            i for i in range(len(solution.routes)) if len(solution.routes[i]) > 0
+        ]
+        vehicle_subproblem = random.sample(
+            vehicle_used, max(1, int(self.fraction_vehicle * len(vehicle_used)))
+        )
+        tasks_subproblem = set()
+        for v in vehicle_subproblem:
+            tasks_subproblem.update(set(solution.routes[v]))
+        others = set(self.problem.tasks_list) - tasks_subproblem
+        return tasks_subproblem, others
+
+
+class VrpTaskConstraintHandler(TasksConstraintHandler):
+    def extract_best_solution_from_last_iteration(
+        self,
+        result_storage: ResultStorage,
+        result_storage_last_iteration: ResultStorage,
+        **kwargs: Any,
+    ) -> Optional[Solution]:
+        if len(result_storage_last_iteration) > 0:
+            sol, _ = result_storage_last_iteration[-1]
+        else:
+            sol, _ = result_storage[-1]
+        return sol
+
+
+def run_lns():
+    file = [f for f in get_data_available() if "C1_2_1.TXT" in f][0]
+    problem = parse_vrptw_file(file)
+    subsolver = CpSatVRPTWSolver(problem=problem)
+    subsolver.init_model(scaling=1, cost_per_vehicle=1000)
+    parameters_cp = ParametersCp.default_cpsat()
+    parameters_cp.nb_process = 16
+    n = NeighborBuilderMix(
+        list_neighbor=[
+            NeighborBuilderVrpTw(problem=problem, fraction_vehicle=0.4),
+            NeighborBuilderSubPart(problem=problem, nb_cut_part=3),
+            NeighborRandom(problem=problem, fraction_subproblem=0.5),
+        ],
+        weight_neighbor=[1 / 3] * 3,
+        verbose=True,
+    )
+    extractors: list[BaseConstraintExtractor] = [
+        SchedulingConstraintExtractor(
+            plus_delta_primary=100,
+            minus_delta_primary=100,
+            plus_delta_secondary=10,
+            minus_delta_secondary=10,
+        ),
+        DummyConstraintExtractor(),
+    ]
+    constraints_extractor = ConstraintExtractorPortfolio(
+        extractors=extractors, weights=[0.8, 0.2]
+    )
+    constraint_handler = VrpTaskConstraintHandler(
+        problem=problem,
+        neighbor_builder=n,
+        constraints_extractor=constraints_extractor,
+    )
+
+    solver = LnsOrtoolsCpSat(
+        problem=problem,
+        subsolver=subsolver,
+        constraint_handler=constraint_handler,
+    )
+    res = solver.solve(
+        callbacks=[
+            ReinitModel(),
+            WarmStartCallbackLastRun(
+                warm_start_last_solution=True, warm_start_best_solution=False
+            ),
+        ],
+        nb_iteration_lns=100,
+        time_limit_subsolver_iter0=10,
+        time_limit_subsolver=TIME_LIMIT_SUBSOLVER,
+        parameters_cp=parameters_cp,
+        skip_initial_solution_provider=True,
+        ortools_cpsat_solver_kwargs={
+            "log_search_progress": False,
+            "fix_variables_to_their_hinted_value": False,
+        },
+    )
+    sol = res.get_best_solution()
+    print(problem.evaluate(sol), problem.satisfy(sol))
+
+
+if __name__ == "__main__":
+    run_lns()
--- src/discrete_optimization/vrptw/solvers/cpsat.py
+++ src/discrete_optimization/vrptw/solvers/cpsat.py
@@ -4,17 +4,27 @@
 import logging
 from typing import Any, Dict, List, Optional
 
-from ortools.sat.python.cp_model import CpSolverSolutionCallback
+from ortools.sat.python.cp_model import CpSolverSolutionCallback, LinearExprT
 
+from discrete_optimization.generic_tasks_tools.enums import StartOrEnd
+from discrete_optimization.generic_tasks_tools.solvers.cpsat import (
+    AllocationCpSatSolver,
+    SchedulingCpSatSolver,
+)
 from discrete_optimization.generic_tools.do_problem import ParamsObjectiveFunction
 from discrete_optimization.generic_tools.do_solver import StatusSolver, WarmstartMixin
 from discrete_optimization.generic_tools.ortools_cpsat_tools import OrtoolsCpSatSolver
-from discrete_optimization.vrptw.problem import VRPTWProblem, VRPTWSolution
+from discrete_optimization.vrptw.problem import (
+    Task,
+    UnaryResource,
+    VRPTWProblem,
+    VRPTWSolution,
+)
 
 logger = logging.getLogger(__name__)
 
 
-class CpSatVRPTWSolver(OrtoolsCpSatSolver, WarmstartMixin):
+class CpSatVRPTWSolver(SchedulingCpSatSolver[Task], WarmstartMixin):
     """
     CP-SAT solver for the Vehicle Routing Problem with Time Windows (VRPTW).
 
@@ -26,6 +36,19 @@ class CpSatVRPTWSolver(OrtoolsCpSatSolver, WarmstartMixin):
         problem (VRPTWProblem): The VRPTW problem instance.
         variables (Dict[str, Any]): Stores CP-SAT model variables.
     """
+
+    def get_task_start_or_end_variable(
+        self, task: Task, start_or_end: StartOrEnd
+    ) -> LinearExprT:
+        # if start_or_end == StartOrEnd.START:
+        return self.variables["t_time"][task]
+
+        # if start_or_end == StartOrEnd.END:
+        #     return self.variables["t_time"][task]+int(self.scaling_factor*self.problem.service_times[task])
+
+    # def get_task_unary_resource_is_present_variable(self, task: Task, unary_resource: UnaryResource) -> LinearExprT:
+    # This is not present in the model unfortunately.
+    #    pass
 
     problem: VRPTWProblem
 
@@ -64,9 +87,6 @@ class CpSatVRPTWSolver(OrtoolsCpSatSolver, WarmstartMixin):
             if i != j
         }
 
-        # vehicle_used[v]: BoolVar, true if vehicle v is used
-        vehicle_used = [self.cp_model.NewBoolVar(f"vehicle_used_{v}") for v in range(k)]
-
         # Time dimension variables: t_time[i] is the time service *starts* at node i
         # We must scale times to use integers
         t_time = [
@@ -92,7 +112,6 @@ class CpSatVRPTWSolver(OrtoolsCpSatSolver, WarmstartMixin):
             "x_arc": x_arc,
             "t_time": t_time,
             "load": load,
-            "vehicle_used": vehicle_used,
         }
 
         # --- Add constraints ---
@@ -116,8 +135,9 @@ class CpSatVRPTWSolver(OrtoolsCpSatSolver, WarmstartMixin):
             == sum(x_arc[i, depot] for i in self.problem.customers)
         )
         # Link to vehicle_used variables
+        nb_vehicles = self.cp_model.NewIntVar(0, k, "nb_vehicles")
         self.cp_model.Add(
-            sum(x_arc[depot, j] for j in self.problem.customers) == sum(vehicle_used)
+            sum(x_arc[depot, j] for j in self.problem.customers) == nb_vehicles
         )
         # No arc from depot to depot
         # self.cp_model.Add(x_arc[depot, depot] == 0)
@@ -132,6 +152,7 @@ class CpSatVRPTWSolver(OrtoolsCpSatSolver, WarmstartMixin):
             ub=int(self.scaling_factor * self.problem.time_windows[depot][1]),
             name="time_return_depot",
         )
+        self.variables["time_return_depot"] = t_time_return_depot
         for i in range(n):
             for j in range(n):
                 if i == j:
@@ -183,8 +204,8 @@ class CpSatVRPTWSolver(OrtoolsCpSatSolver, WarmstartMixin):
             == sum(x_arc[i, j] * scaled_distances[i, j] for i, j in x_arc)
         )
         # Number of vehicles
-        nb_vehicles = self.cp_model.NewIntVar(0, k, "nb_vehicles")
-        self.cp_model.Add(nb_vehicles == sum(vehicle_used))
+        self.variables["nb_vehicles"] = nb_vehicles
+        self.variables["total_distance"] = total_distance
         # Lexicographic objective: 1. Min vehicles, 2. Min distance
         # We use a large constant to separate the objectives
         self.cp_model.Minimize(nb_vehicles * cost_per_vehicle + total_distance)
@@ -247,43 +268,75 @@ class CpSatVRPTWSolver(OrtoolsCpSatSolver, WarmstartMixin):
                 if not successor_found or current_node == depot:
                     break  # Should not happen unless route ends at depot
             routes.append(route)
-        return VRPTWSolution(problem=self.problem, routes=routes)
+        sol = VRPTWSolution(
+            problem=self.problem, routes=routes, scaling=self.scaling_factor
+        )
+        sol._times = [
+            cpsolvercb.Value(self.variables["t_time"][i])
+            for i in range(len(self.variables["t_time"]))
+        ]
+        sol._time_return = cpsolvercb.Value(self.variables["time_return_depot"])
+        sol._total_distance = cpsolvercb.Value(self.variables["total_distance"])
+        return sol
 
     def set_warm_start(self, solution: VRPTWSolution) -> None:
         """
         Provides a warm start hint to the CP-SAT solver from an existing solution.
         """
         if self.cp_model is None:
-            self.init_model()
-        if self.cp_model is None:
             raise RuntimeError(
                 "self.cp_model must not be None after self.init_model()."
             )
-
         self.cp_model.ClearHints()
         logger.info("Setting warm start from solution.")
-
         x_arc = self.variables["x_arc"]
+        load_var = self.variables["load"]
+        self.cp_model.AddHint(load_var[self.problem.depot_node], 0)
         depot = self.problem.depot_node
-
         # Hint arc variables
+        arcs = {key: 0 for key in x_arc}
+        total_distance = 0
+        nb_vehicle = 0
         for route in solution.routes:
             if not route:
                 continue
-
-            # Arc from depot
-            self.cp_model.AddHint(x_arc[depot, route[0]], 1)
-
+            nb_vehicle += 1
+            arcs[(depot, route[0])] = 1
             # Arcs between customers
+            cumul_load = int(self.problem.demands[route[0]])
+            total_distance += int(
+                self.scaling_factor * self.problem.distance_matrix[depot, route[0]]
+            )
+            self.cp_model.add_hint(load_var[route[0]], cumul_load)
             for i in range(len(route) - 1):
                 u, v = route[i], route[i + 1]
-                self.cp_model.AddHint(x_arc[u, v], 1)
-
+                cumul_load += int(self.problem.demands[v])
+                total_distance += int(
+                    self.scaling_factor * self.problem.distance_matrix[u, v]
+                )
+                self.cp_model.add_hint(load_var[v], cumul_load)
+                arcs[(u, v)] = 1
+            total_distance += int(
+                self.scaling_factor * self.problem.distance_matrix[route[-1], depot]
+            )
             # Arc back to depot
-            self.cp_model.AddHint(x_arc[route[-1], depot], 1)
-
-        # Hint time variables (if evaluated)
-        if solution.start_service_times:
+            arcs[(route[-1], depot)] = 1
+        for key in arcs:
+            self.cp_model.AddHint(x_arc[key], arcs[key])
+        self.cp_model.add_hint(
+            self.variables["time_return_depot"], solution._time_return
+        )
+        self.cp_model.add_hint(self.variables["nb_vehicles"], nb_vehicle)
+        print(solution._total_distance, " vs ", total_distance)
+        self.cp_model.add_hint(
+            self.variables["total_distance"], solution._total_distance
+        )
+        if getattr(solution, "_times", None) is not None:
+            ts = getattr(solution, "_times")
+            t_time = self.variables["t_time"]
+            for i in range(len(ts)):
+                self.cp_model.AddHint(t_time[i], ts[i])
+        elif solution.start_service_times:
             t_time = self.variables["t_time"]
             for v_idx, route in enumerate(solution.routes):
                 starts = solution.start_service_times[v_idx]
