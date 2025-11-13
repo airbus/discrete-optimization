e478223755814e06dcc2a899d33cfb24acaa9fe8
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2025-11-13 15:53:23 +0100

    Increase time limit for vrptw cpast test

--- tests/vrptw/test_cpsat.py
+++ tests/vrptw/test_cpsat.py
@@ -1,6 +1,9 @@
 #  Copyright (c) 2025 AIRBUS and its affiliates.
 #  This source code is licensed under the MIT license found in the
 #  LICENSE file in the root directory of this source tree.
+from discrete_optimization.generic_tools.callbacks.early_stoppers import (
+    NbIterationStopper,
+)
 from discrete_optimization.generic_tools.cp_tools import ParametersCp
 from discrete_optimization.vrptw.parser import get_data_available, parse_vrptw_file
 from discrete_optimization.vrptw.problem import VRPTWProblem, VRPTWSolution
@@ -13,9 +16,9 @@ def test_cpsat():
     solver = CpSatVRPTWSolver(problem=problem)
     solver.init_model(scaling=10, cost_per_vehicle=1000)
     res = solver.solve(
-        callbacks=[],
+        callbacks=[NbIterationStopper(1)],
         parameters_cp=ParametersCp.default_cpsat(),
-        time_limit=10,
+        time_limit=100,
         ortools_cpsat_solver_kwargs={"log_search_progress": True},
     )
     sol = res[-1][0]
