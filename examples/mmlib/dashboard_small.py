f99ec147d00edb281a57a24bdc6c56e1709b90c4
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2026-05-19 14:56:34 +0200

    Add a smaller version of mmlib study making use of new study object

---
+++ examples/mmlib/dashboard_small.py
@@ -0,0 +1,19 @@
+#  Copyright (c) 2026 AIRBUS and its affiliates.
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+from discrete_optimization.generic_tools.dashboard import Dashboard
+from discrete_optimization.generic_tools.study import Hdf5Database
+
+n_instances = 40
+time_limit = 120
+study_name = f"mmlibtest-{n_instances}-largest-instance-{time_limit}s"
+
+
+if __name__ == "__main__":
+    # retrieve data
+    with Hdf5Database(f"{study_name}.h5") as database:
+        results = database.load_results()
+
+    # launch dashboard with this data
+    app = Dashboard(results=results)
+    app.run()
