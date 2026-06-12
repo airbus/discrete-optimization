dab03b7f4b42ac4f4671a0c90b20107deeabca5e
poveda_g <guillaume.poveda@airbus.com>
2026-06-12 18:54:11 +0200

    adding optal model

--- src/discrete_optimization/flex_scheduling/solvers/heuristic_solver.py
+++ src/discrete_optimization/flex_scheduling/solvers/heuristic_solver.py
@@ -1,3 +1,6 @@
+#  Copyright (c) 2026 AIRBUS and its affiliates.
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
 from typing import Any, Hashable, Optional
 
 import numpy as np
