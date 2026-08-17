210d3e1d9868357d844a811acff85a1903f4028a
poveda_g <guillaume.poveda@airbus.com>
2026-08-17 17:00:25 +0200

    finish implementation of cpmpy solver, and associated example/test for the shop problems

--- examples/shop/run_fjsp.py
+++ examples/shop/run_fjsp.py
@@ -1,6 +1,9 @@
 #  Copyright (c) 2024-2025 AIRBUS and its affiliates.
 #  This source code is licensed under the MIT license found in the
 #  LICENSE file in the root directory of this source tree.
+import os
+
+os.environ["HX_LICENSE_PATH"] = "/Users/poveda_g/Documents/hexaly/licence.dat"
 
 import logging
 
@@ -8,12 +11,14 @@ from discrete_optimization.generic_tools.callbacks.loggers import ProblemEvaluat
 from discrete_optimization.generic_tools.cp_tools import ParametersCp
 from discrete_optimization.shop.fjsp.parser import get_data_available, parse_file
 from discrete_optimization.shop.fjsp.solvers.cpsat import CpSatFjspSolver
+from discrete_optimization.shop.solvers.cpmpy import CpmpyShopSolver
 from discrete_optimization.shop.solvers.greedy import GreedyShopSolver
+from discrete_optimization.shop.utils import plot_shop_solution, plt
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 
-def run_cpsat_jsp():
+def run_cpsat_fjsp():
     file_path = get_data_available()[4]
     # file_path = [f for f in get_data_available() if "abz6" in f][0]
     problem = parse_file(file_path)
@@ -44,7 +49,7 @@ def run_cpsat_jsp():
     print(problem.evaluate(sol))
 
 
-def run_cpsat_jsp_warm_start():
+def run_cpsat_fjsp_warm_start():
     file_path = get_data_available()[4]
     # file_path = [f for f in get_data_available() if "abz6" in f][0]
     problem = parse_file(file_path)
@@ -74,5 +79,39 @@ def run_cpsat_jsp_warm_start():
     print(problem.evaluate(sol))
 
 
+def run_cpmpy_fjsp():
+    file_path = get_data_available()[4]
+    # file_path = [f for f in get_data_available() if "abz6" in f][0]
+    problem = parse_file(file_path)
+    print("File path ", file_path)
+    print(
+        "Problem with ",
+        problem.n_jobs,
+        " jobs, ",
+        problem.n_all_jobs,
+        " subjobs, and ",
+        problem.n_machines,
+        " machines",
+    )
+    solver = CpmpyShopSolver(problem=problem, solver_name="ortools")
+    solver.init_model(link_mode_to_duration=True)
+    p = ParametersCp.default_cpsat()
+    p.nb_process = 10
+    res = solver.solve(
+        parameters_cp=p,
+        callbacks=[
+            ProblemEvaluateLogger(
+                step_verbosity_level=logging.INFO, end_verbosity_level=logging.INFO
+            )
+        ],
+        time_limit=10,
+    )
+    sol = res.get_best_solution_fit()[0]
+    assert problem.satisfy(sol)
+    print(problem.evaluate(sol))
+    plot_shop_solution(sol)
+    plt.show()
+
+
 if __name__ == "__main__":
-    run_cpsat_jsp_warm_start()
+    run_cpsat_fjsp()
--- src/discrete_optimization/shop/solvers/cpmpy.py
+++ src/discrete_optimization/shop/solvers/cpmpy.py
@@ -6,11 +6,21 @@ from typing import Any
 import cpmpy as cp
 
 from discrete_optimization.generic_tools.cpmpy_tools import CpmpySolver
-from discrete_optimization.generic_tools.do_problem import ParamsObjectiveFunction
-from discrete_optimization.shop.base import CommonShopProblem
+from discrete_optimization.generic_tools.do_problem import (
+    ParamsObjectiveFunction,
+)
+from discrete_optimization.generic_tools.hyperparameters.hyperparameter import (
+    CategoricalHyperparameter,
+)
+from discrete_optimization.shop.base import AnyShopSolution, CommonShopProblem
 
 
 class CpmpyShopSolver(CpmpySolver):
+    hyperparameters = [
+        CategoricalHyperparameter(
+            name="link_mode_to_duration", choices=[True, False], default=False
+        )
+    ]
     problem: CommonShopProblem
 
     def __init__(
@@ -32,9 +42,11 @@ class CpmpyShopSolver(CpmpySolver):
     def init_model(self, **kwargs: Any) -> None:
         # optional parameters
         kwargs = self.complete_with_default_hyperparameters(kwargs)
+        link_mode_to_duration = kwargs["link_mode_to_duration"]
         self._max_time: int | None = kwargs.get(
             "max_time", None
         )  # update the upper bound for makespan
+        self.model = cp.Model()
         # Decision `start[j]`: integer start time for each job `j`
         start = cp.intvar(
             0,
@@ -49,7 +61,102 @@ class CpmpyShopSolver(CpmpySolver):
             shape=self.problem.n_all_jobs,
             name="end",
         )
-        pass
+        max_dur = self.problem.get_max_duration_of_tasks()
+        duration = cp.intvar(
+            lb=0, ub=max_dur, name="duration", shape=self.problem.n_all_jobs
+        )
+        modes = {}
+        for t in self.problem.tasks_list:
+            for m in self.problem.get_task_modes(t):
+                modes[(t, m)] = cp.boolvar(1, name=f"mode_{t}_{m}")
+                if link_mode_to_duration:
+                    self.model += [
+                        modes[(t, m)].implies(
+                            duration[self.problem.get_index_from_task(t)]
+                            == self.problem.get_task_mode_duration(t, m)
+                        )
+                    ]
+            self.model += [
+                cp.sum([modes[(t, m)] for m in self.problem.get_task_modes(t)]) == 1
+            ]
+        self.variables["start"] = start
+        self.variables["end"] = end
+        self.variables["modes"] = modes
+        successors = self.problem.get_precedence_constraints()
+        for pred in successors:
+            for succ in successors[pred]:
+                self.model += [
+                    start[self.problem.get_index_from_task(succ)]
+                    >= end[self.problem.get_index_from_task(pred)]
+                ]
+        for set_task in self.problem.get_no_overlap():
+            self.model += [
+                cp.NoOverlap(
+                    start=[
+                        start[self.problem.get_index_from_task(task)]
+                        for task in set_task
+                    ],
+                    end=[
+                        end[self.problem.get_index_from_task(task)] for task in set_task
+                    ],
+                    duration=[
+                        duration[self.problem.get_index_from_task(task)]
+                        for task in set_task
+                    ],
+                )
+            ]
+        for machine in self.problem.cumulative_resources_list:
+            task_mode = [
+                ((t, m), self.problem.get_task_mode_duration(t, m))
+                for t, m in modes
+                if self.problem.get_cumulative_resource_consumption(machine, t, m) > 0
+            ]
+            self.model += [
+                cp.NoOverlapOptional(
+                    start=[
+                        start[self.problem.get_index_from_task(tm[0])]
+                        for tm, _ in task_mode
+                    ],
+                    duration=[d for _, d in task_mode],
+                    end=[
+                        end[self.problem.get_index_from_task(tm[0])]
+                        for tm, _ in task_mode
+                    ],
+                    is_present=[modes[tm] for tm, _ in task_mode],
+                )
+            ]
+        self.model.minimize(cp.max(end))
 
-    def create_vars(self):
-        pass
+    def retrieve_current_solution(self) -> AnyShopSolution:
+        starts = self.variables["start"].value()
+        ends = self.variables["end"].value()
+        schedule = [
+            [
+                (
+                    starts[self.problem.get_index_from_task((i, k))],
+                    ends[self.problem.get_index_from_task((i, k))],
+                )
+                for k in range(self.problem.nb_subjob_per_job[i])
+            ]
+            for i in range(self.problem.n_jobs)
+        ]
+        machine_index = [
+            [None for _ in range(self.problem.nb_subjob_per_job[i])]
+            for i in range(self.problem.n_jobs)
+        ]
+        recipe_index = [
+            [None for _ in range(self.problem.nb_subjob_per_job[i])]
+            for i in range(self.problem.n_jobs)
+        ]
+        for t, m in self.variables["modes"]:
+            val = self.variables["modes"][(t, m)].value()
+            if val == 1:
+                recipe_index[t[0]][t[1]] = m
+                machine_index[t[0]][t[1]] = self.problem.mode2machine[t][m]
+
+        return AnyShopSolution(
+            problem=self.problem,
+            schedule=schedule,
+            machine_index=machine_index,
+            recipe_index=recipe_index,
+        )
