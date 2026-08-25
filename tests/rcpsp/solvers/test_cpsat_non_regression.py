da78d5cd09edcadb7aa6caa306e17715a0c42613
Nolwen <nolwen.huet@imacs.fr>
2026-08-25 16:12:02 +0200

    Add non-regression tests on cpsat solvers for some scheduling pbs
    Classes of problems concerned by the non-regression tests:
    - rcpsp
    - rcpsp_ms
    - jsp
    - fjsp
    - workforce/scheduling
    
    Some utility fixtures are defined in tests/conftest.py to ease the
    creation of new non-regression tests.
    
    Per test-module it defines
    - a csv database file (module test name + _db.csv"
    - a check_nonregression_fn function, which either tests against previous
      test runs found in the csv (default behaviour) or populate the csv
      with the current version of problems/solvers. You need for that to
      define and set  `NONREGRESSION_POPULATE_DATABASE=True` in the
      concerned test module.
    - this checks that
      - optimal cases stay optimal with same objective values
      - satified cases are either improved or not worse that the stored
        objectives + margin (5% by default). You can set the margin to a
        different value by setting a variable `NONREGRESSION_MARGIN_OBJECTIVE` at test module level.
      - in case of multiobjectives, it is checked that the first components
        are equal and the next one is below the stored value + margin
    - the csv database also stores the optimization sense (either
      maximization or minimzation) to translate properly the previous checks
      in case of maximization
    - the usecase are identified by the name given by pytest or pytest-cases
      and retrieve via `request.node.name`

---
+++ tests/rcpsp/solvers/test_cpsat_non_regression.py
@@ -0,0 +1,40 @@
+#  Copyright (c) 2025 AIRBUS and its affiliates.
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+
+from pytest_cases import parametrize_with_cases
+
+NONREGRESSION_POPULATE_DATABASE = False
+"""Whether to create the database or use it to test against it.
+
+- True: we create the non-regression database
+- False: we compare the results with the ones previously stored with the non-regression database
+
+"""
+
+
+@parametrize_with_cases("problem", prefix="problem_")
+@parametrize_with_cases(
+    "solver_cls, solver_kwargs, objective_fn, mode_optim", prefix="solver_"
+)
+def test_rcpsp_solver(
+    problem,
+    solver_cls,
+    solver_kwargs,
+    objective_fn,
+    mode_optim,
+    request,
+    check_nonregression_fn,
+):
+    solver = solver_cls(problem=problem, **solver_kwargs)
+    solver.init_model(**solver_kwargs)
+    res = solver.solve(**solver_kwargs)
+    sol, fit = res[-1]
+    objective = objective_fn(sol)
+    test_id = request.node.nodeid
+    status = solver.status_solver
+
+    # compare with previous runs (or populate the nonregression database)
+    check_nonregression_fn(
+        test_id=test_id, status=status, objective=objective, mode_optim=mode_optim
+    )
