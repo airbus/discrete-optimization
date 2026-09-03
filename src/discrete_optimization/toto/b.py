02421b9ad64bf9f637cec5658d4416e86ecc5abf
Nolwen <nolwen.huet@imacs.fr>
2026-09-03 22:34:53 +0200

    remove collection errors

---
+++ src/discrete_optimization/toto/b.py
@@ -0,0 +1,8 @@
+#  Copyright (c) 2026 AIRBUS and its affiliates.
+#  This source code is licensed under the MIT license found in the
+#  LICENSE file in the root directory of this source tree.
+
+
+class B:
+    def __init__(self, x):
+        self.x = x / 0
